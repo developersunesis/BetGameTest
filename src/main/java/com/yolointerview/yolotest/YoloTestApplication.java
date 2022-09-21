970f27105bdcced899186c812b4cb3d055593758
developersunesis <developersunesis@gmail.com>
2022-09-21 16:27:10 +0100

    Initial Commit: Project created using Spring Intialzer

---
+++ src/main/java/com/yolointerview/yolotest/YoloTestApplication.java
@@ -0,0 +1,13 @@
+package com.yolointerview.yolotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoloTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoloTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yolointerview/yolotest/YoloTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yolointerview.yolotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoloTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
